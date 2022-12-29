95b8b3581c5f984e733305acf10414024c60fa7c
hawlader44c <mdbadolhawlader44c@gmail.com>
2022-12-29 19:31:52 +0600

    news-info-carosole

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import NewsInfo from "./NewsInfo";
+// import KhaborInfo from "./KhaborInfo";
 function App() {
   return (
     <div className="App">
       <NewsInfo />
+      {/* <KhaborInfo /> */}
     </div>
   );
 }
--- src/NewsInfo.js
+++ src/NewsInfo.js
@@ -1,49 +1,100 @@
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+import { useRef, useCallback } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "./styles.css";
+// import { Navigation } from "swiper";
 import styles from "./styles/style.module.css";
 const NewsInfo = () => {
+  const sliderRef = useRef(null);
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, []);
   return (
     <>
-      <div className="w-50 bg-dark" id={styles.infoNews}>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/image1.png"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/image2.png"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/image1.png"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+      <div className="container">
+        <div className="d-lg-flex">
+          <div className="swiper">
+            <Swiper slidesPerView={"auto"} spaceBetween={30}>
+              <SwiperSlide>
+                <div className="card" id={styles.hw}>
+                  <div class="card-body">
+                    <img alt="" src="./images/image1.png" />
+                    <h2>Title</h2>
+                    <p class="card-text">
+                      Aliquam massa scelerisque varius nulla vitae mattis ac
+                      nunc dignissim. Posuere consequat nibh varius auctor sit
+                      netus diam sodales in.
+                    </p>
+                    <button className="bg-dark text-white">Read More</button>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="card" id={styles.hw}>
+                  <div class="card-body">
+                    <img alt="" src="./images/image1.png" />
+                    <h2>Title</h2>
+                    <p class="card-text">
+                      Aliquam massa scelerisque varius nulla vitae mattis ac
+                      nunc dignissim. Posuere consequat nibh varius auctor sit
+                      netus diam sodales in.
+                    </p>
+                    <button className="bg-dark text-white">Read More</button>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="card" id={styles.hw}>
+                  <div class="card-body">
+                    <img alt="" src="./images/image2.png" />
+                    <h2>Title</h2>
+                    <p class="card-text">
+                      Aliquam massa scelerisque varius nulla vitae mattis ac
+                      nunc dignissim. Posuere consequat nibh varius auctor sit
+                      netus diam sodales in.
+                    </p>
+                    <button className="bg-dark text-white">Read More</button>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="card" id={styles.hw}>
+                  <div class="card-body">
+                    <img alt="" src="./images/image1.png" />
+                    <h2>Title</h2>
+                    <p class="card-text">
+                      Aliquam massa scelerisque varius nulla vitae mattis ac
+                      nunc dignissim. Posuere consequat nibh varius auctor sit
+                      netus diam sodales in.
+                    </p>
+                    <button className="bg-dark text-white">Read More</button>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+          <div id={styles.infoall}>
+            <p id={styles.topnews}>Top News</p>
+            <p id={styles.topnewspara}>
+              Id blandit convallis ipsum commodo fermentum urna pellentesque.
+              Consectetur fringilla sit sed morbi ultrices pellentesque eget
+              elementum pharetra.
+            </p>
+          </div>
+          <div className="swiper-button-prev" onClick={handlePrev}>
+            Click
+          </div>
+          <div className="next-arrow" onClick={handleNext} />
+        </div>
       </div>
     </>
   );
